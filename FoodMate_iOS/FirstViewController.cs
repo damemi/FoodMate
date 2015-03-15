705701515069cc8181ad45a75fe6e3b9edab87aa
Jazmine Olinger <olingj@rpi.edu>
2015-03-15 17:08:13 -0400

    iOS facebook auth

--- FoodMate_iOS/FirstViewController.cs
+++ FoodMate_iOS/FirstViewController.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.IO;
 using System.Drawing;
 
 using Foundation;
 using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Auth;
+using Parse;
+using Newtonsoft.Json.Linq;
+
 
 namespace FoodMate_iOS
 {
@@ -52,6 +59,54 @@ namespace FoodMate_iOS
 		}
 
 		#endregion
+
+		void LoginToFacebook ()
+		{
+
+			String appID = File.ReadAllText("appID.txt");
+			var auth = new OAuth2Authenticator (
+				clientId: appID,
+				scope: "",
+				authorizeUrl: new Uri ("https://m.facebook.com/dialog/oauth/"),
+				redirectUrl: new Uri ("http://www.facebook.com/connect/login_success.html"));
+
+			// If authorization succeeds or is canceled, .Completed will be fired.
+			auth.Completed += LoginComplete;
+
+			UIViewController vc = auth.GetUI ();
+			PresentViewController (vc, true, null);
+		}
+
+		public async void LoginComplete( object sender, AuthenticatorCompletedEventArgs e )
+		{
+			// We presented the UI, so it's up to us to dismiss it.
+			DismissViewController (true, null);
+
+			if (!e.IsAuthenticated) {
+				Console.WriteLine ("Not Authorised");
+				return;
+			}
+
+			var accessToken = e.Account.Properties["access_token"].ToString();
+			var expiresIn = Convert.ToDouble(e.Account.Properties["expires_in"]);
+			var expiryDate = DateTime.Now + TimeSpan.FromSeconds( expiresIn );
+
+			// Now that we're logged in, make a OAuth2 request to get the user's id.
+			var request = new OAuth2Request ("GET", new Uri ("https://graph.facebook.com/me"), null, e.Account);
+			var response = await request.GetResponseAsync();
+			var obj = JObject.Parse (response.GetResponseText());
+
+			var id = obj["id"].ToString().Replace("\"",""); // Id has extraneous quotation marks
+
+			var user = await ParseFacebookUtils.LogInAsync(id, accessToken,expiryDate);
+	
+		}
+
+		partial void UIButton14_TouchUpInside (UIButton sender)
+		{
+			LoginToFacebook();
+
+		}
 	}
 }
 
